0f4c4e46d015e0c72d9e16000984148c77aeff63
kode0x <kode0x.work@gmail.com>
2026-08-02 10:53:00 +0530

    feat: add AlexNet implementations in TensorFlow and PyTorch

---
+++ AlexNet/AlexNet_TF.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+
+class AlexNet(tf.keras.Model):
+    def __init__(self):
+        super().__init__()
+        self.features = tf.keras.Sequential(
+            [
+                layers.Conv2D(96, kernel_size=11, strides=4, activation="relu", input_shape=(227, 227, 3)),
+                layers.MaxPooling2D(pool_size=3, strides=2),
+                layers.Conv2D(256, kernel_size=5, padding="same", activation="relu"),
+                layers.MaxPooling2D(pool_size=3, strides=2),
+                layers.Conv2D(384, kernel_size=3, padding="same", activation="relu"),
+                layers.Conv2D(384, kernel_size=3, padding="same", activation="relu"),
+                layers.Conv2D(256, kernel_size=3, padding="same", activation="relu"),
+                layers.MaxPooling2D(pool_size=3, strides=2),
+            ]
+        )
+        self.classifier = tf.keras.Sequential(
+            [
+                layers.Flatten(),
+                layers.Dense(4096, activation="relu"),
+                layers.Dropout(0.5),
+                layers.Dense(4096, activation="relu"),
+                layers.Dropout(0.5),
+                layers.Dense(1000),
+            ]
+        )
+
+    def call(self, inputs):
+        x = self.features(inputs)
+        return self.classifier(x)
